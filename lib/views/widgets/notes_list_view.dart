d5d71c3c0893b26452802854d39f61fa96a310a0
adel7ussein <aadelshabib67009@gmail.com>
2023-11-28 23:10:00 +0200

    Create Edit Note View

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -7,11 +7,13 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return const Padding(
-        padding: EdgeInsets.symmetric(vertical: 4.0),
-        child: NoteItem(),
-      );
-    });
+    return ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+            child: NoteItem(),
+          );
+        });
   }
 }
